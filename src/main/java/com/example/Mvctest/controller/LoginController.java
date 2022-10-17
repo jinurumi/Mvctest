da5ccc597068a99d3b53a6f71643b1bf06bf990e
Jinhye <cindy8946@gmail.com>
2022-10-17 17:47:11 +0900

    [docs] 초안3

--- src/main/java/com/example/Mvctest/controller/LoginController.java
+++ src/main/java/com/example/Mvctest/controller/LoginController.java
@@ -5,6 +5,8 @@ public class LoginController {
     //전달받은 요청을 LoginService 로 전달하고 서비스에서 로직을 수행한다
 
 
+
+
     //Api 규격 맞추기
 
 
--- src/main/java/com/example/Mvctest/controller/WebController.java
+++ src/main/java/com/example/Mvctest/controller/WebController.java
@@ -8,10 +8,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
 
     //GetMapping으로 리다이렉트 기능만 수행
-    @GetMapping("/")
+    @GetMapping("/index")
     public String indexView() {
-        return "redirect:/index";
+        //if 로그인 성공
+        //홈화면에 로그인한 사용자 이름 띄우기
+        return "redirect:/";
     }
+
+    @GetMapping("/login")
+    public String loginView() {
+        //if(없는 사용자면) {
+        //alert("없습니다") }
+        //else if (있는 사용자면) {
+        return "redirect:/";
+        //}
+    }
+
+
+
+
+    /* index 페이지에서 로그인하기 성공했으면 홈 화면으로 리다이렉트해서 홈화면에 로그인한 사용자 이름 띄우기
+       login 페이지에는 이름, 나이/입력칸 두개. 입력완료 버튼.을 누르면 없는 사용자면 alert("없습니다.") 있는 사용자면 홈화면으로 리다이렉트 */
+
+
 
 
 
