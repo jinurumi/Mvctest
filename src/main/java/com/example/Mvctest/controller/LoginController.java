1c85c1dcec0b679ca74454733d8df2f614c38305
jerry8534 <jerryjjinu@gmail.com>
2022-10-18 01:53:09 +0900

    aa
    보낼때 stringify 해서 보내면 400 에러 뜨고 type post로 하면 405에러 뜨는데 그거는 HTTP Response Headers allows 를 post로 바꿔주면 될거 같은데 어떻게 하는지 모르겠어요ㅠㅠ 그리고 지금 연결은 됐는데 어차피 돌아오는게 text로 html 그대로 와서 로그인 성공이 안돼요 근데 또 JSON으로 받으면 오류나는데 그건 콘솔에서도 못알아보게 나와서.... 이거 제가 잘못 짜서 어차피 안될거 같아요ㅠㅠ

--- src/main/java/com/example/Mvctest/controller/LoginController.java
+++ src/main/java/com/example/Mvctest/controller/LoginController.java
@@ -1,29 +1,31 @@
 package com.example.Mvctest.controller;
 
 import com.example.Mvctest.User;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.logging.Logger;
-
+@CrossOrigin(origins = "<http://localhost:8877>")
 @RestController
 @RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
 public class LoginController {
 
-    @PostMapping(value = "/login", produces = "application/json")
+    @GetMapping(value = "/login", produces = "application/json")
     public String login(@RequestBody User user, Model model) {
         System.out.println("user" + user);
         User user1 = new User("jinwoo", "29");
         User user2 = new User("jinhye", "25");
         System.out.println(user.getName());
+        String result = "";
         if(user.getName().equals(user1.getName()) || user.getName().equals(user2.getName())){
             model.addAttribute("name", user.getName());
+            result = "success";
         }
         else{
             model.addAttribute("name", "unknown");
+            result = "fail";
         }
-        return "200";
+        return result;
     }
 }
